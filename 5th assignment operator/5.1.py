f0de90106c90e2e262ad77930aefcae455701f61
sakariya-shwet <sakariyashwet@gmail.com>
2026-04-20 11:28:00 +0530

    Add files via upload

---
+++ 5th assignment operator/5.1.py
@@ -0,0 +1,8 @@
+# increment and decrement a number
+
+a= int(input("Enter your number:-"))
+
+a+=10000
+print("Increment number is:-",a)
+
+             
---
+++ 5th assignment operator/5.2.py
@@ -0,0 +1,16 @@
+#use +=,-=,*= in calculation
+
+a=int(input("Enter your number"))
+
+a +=123
+
+print("Increment number is:",a)
+
+a=-123
+
+print("Decrement number :",a)
+
+a*=123
+
+print("multiplication :",a)
+
