d796350be6622c2cea6886ac1159a19d0e252eec
sakariya-shwet <sakariyashwet@gmail.com>
2026-04-20 11:13:05 +0530

    Add files via upload

---
+++ 4th logical operators/4.2.py
@@ -0,0 +1,15 @@
+#LOGIN SYSTEM="shwet"
+
+a="shwet"
+b="1234"
+
+a=int(input("Enter your username"))
+b=int(input("Enter your password"))
+
+if username==a and password==b:
+    print("log in successfully")
+else:
+    print("log in failed")
+ 
+    
+
