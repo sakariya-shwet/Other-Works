1686cea66a15ec45a81a4211111e2cbf508291e0
sakariya-shwet <sakariyashwet@gmail.com>
2026-04-23 10:38:49 +0530

    Add files via upload

---
+++ 1.1 term 2.py
@@ -0,0 +1,15 @@
+# take an integer input and convert it to:
+
+# float to string
+
+
+str_float _num=3.15
+
+str_float=str(str_float_num)
+
+print(f"(str_sloat_num)
+      -> str: {str_float},type:
+      { type(str_float)}")
+
+
+
