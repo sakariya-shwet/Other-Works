f362e98feccb9e631d4391481ea817d961d947cd
sakariya-shwet <sakariyashwet@gmail.com>
2026-04-27 10:57:11 +0530

    Add files via upload

---
+++ Type conversion & string manipulation/program.py
@@ -0,0 +1,17 @@
+print("Hello welcome to chat i am artificial inteligence")
+name=input("What is your name:-")
+print("Nice to meet you",name)
+age=int(input("How old are you?"))
+if age<18:
+    print("Nice you are now student keep study")
+else:
+    print("Wow so you are now doing your career work")
+choose=input("what will you choose after 10th either science or commerce")
+if choose== "science":
+    print("that's nice keep it up")
+else :
+    print("Amazing commerce is best stream you can best perform!")
+print("commerce is one of the best stream keep it up")
+print("Intresting choice do you best!")
+print("Thank's for chating",name)
+
