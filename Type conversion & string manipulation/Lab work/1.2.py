8e81afe507a965fcb97b07bee58ad25fb3491955
sakariya-shwet <sakariyashwet@gmail.com>
2026-04-27 11:22:14 +0530

    Add files via upload

---
+++ Type conversion & string manipulation/Lab work/1.2.py
@@ -0,0 +1,18 @@
+# using aep
+
+print("Apple","banana","Watermelon","mango",sep="|")
+
+# using end
+
+print ("Hello",end=" ")
+print("World")
+
+# age name and hobby
+name=input("Enter your name")
+age=input("Enter your age")
+hobby=input("Enter your hobby")
+
+print(f"Hello my name is {name} my age is {age} my hobby is {hobby}")
+
+
+
